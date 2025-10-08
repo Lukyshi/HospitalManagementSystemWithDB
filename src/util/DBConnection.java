2bb5dc4073bc0bb5fa501db6886a782e86f8ae59
Luiz pogi <johnluizvbangsoy14@gmail.com>
2025-10-09 00:14:14 +0800

    hide the db connection

--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -1,21 +1,34 @@
 package util;
 
+import java.io.FilterInputStream;
 import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+//import file
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 
 public class DBConnection {
-    private static final String url = "jdbc:mysql://localhost:3306/hospitaldb";
-    private static final String root = "root";
-    private static final String password = "Luiz010124";
+    private static final String CONFIG_FILE = "src/config.properties";
 
     public static Connection getConnection() {
+        Connection connection = null;
         try {
-            return DriverManager.getConnection(url, root, password);
-        }catch (SQLException e) {
-            System.out.println("Database connection failed " + e.getMessage());
-            return null;
+            Properties props = new Properties();
+            props.load(new FileInputStream(CONFIG_FILE));
+
+            String url = props.getProperty("db.url");
+            String username = props.getProperty("db.username");
+            String password = props.getProperty("db.password");
+
+            connection = DriverManager.getConnection(url, username, password);
+
+        }catch (IOException | SQLException e) {
+            System.out.println("Database connection failed: " + e.getMessage());
         }
+        return connection;
     }
 }
